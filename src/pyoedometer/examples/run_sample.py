117b3ba5638c4a059ac55c238cc81feae7495c72
tingeman <thin@dtu.dk>
2025-06-01 09:16:19 +0200

    Moved userinput on processing steps from run_sample.py script to config_sample.yml
    Refactored config_sample.py for better orgainization, and added explanative comments
    Refactored code to first to simple interpretation of all steps, then do detailed processing of specified steps.

--- src/pyoedometer/examples/run_sample.py
+++ src/pyoedometer/examples/run_sample.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 
 import pandas as pd
 
+try:
+    import ipdb as pdb
+except ImportError:
+    import pdb
+
 import pyoedometer
 import pyoedometer.pyoedio as pyoedio
 reload(pyoedometer)
@@ -16,48 +21,95 @@ from pyoedometer import *
 
 
 # Read information from files
-info = read_config('./config_sample.yml')
+info = read_config('sample_data/config_sample.yml')
+#config = info['config']          # configuration of the measurement setup
+sample_info = info['sample']     # information about the sample 
+history = info['history']        # history of load steps and nominal (set point) temperatures
+interpret = info['interpret']    # interpretation parameters for each step
+latex_info = info['latex']       # information for LaTeX file generation
 
-config = info['config']
-sample_info = info['sample']
-history = info['history']
-interpret = info['interpret']
-latex_info = info['latex']
+lvdt_dat = read_lvdt_data(info['lvdt'], info['sample'])    # Read LVDT data
+pt100T = read_pt100_data(info['pt100'])                    # Read PT100 temperature data
+hoboT = read_hobo_data(info['hobo'])                       
 
-lvdt_dat = read_lvdt_data(info['lvdt'], info['sample'])
-pt100T = read_pt100_data(info['pt100'])
-hoboT = read_hobo_data(info['hobo'])
+#------------------------------------------------------------
+# Set up processing parameters
+#------------------------------------------------------------
 
-steps_to_interpret = []
-steps_to_plot =  [] 
-steps_to_overview = []
-steps_to_save = []
+if True:
+    # Get processing parameters from the configuration file
+    steps_to_interpret = info['processing']['steps_to_interpret']  
+    steps_to_plot =  info['processing']['steps_to_plot_timecurve'] 
+    steps_to_overview = info['processing']['steps_to_plot_overview'] 
+    steps_to_save = info['processing']['steps_to_save']            
 
-# Specify which steps to process or plot
-#steps_to_interpret = []
-#steps_to_interpret = [13,14,15,16] 
-steps_to_interpret = [d['step'] for d in interpret]
+    plot_full_timeseries = info['processing']['plot_full_timeseries']
+    plot_full_timeseries_temp = info['processing']['plot_temperature_with_full_timeseries'] 
+    close_figs_after_save = info['processing']['close_figs_after_saving']       
 
-#steps_to_plot = []
-#steps_to_plot =  [14,14.1,15] 
-#steps_to_plot = [13,14,15,16]
-steps_to_plot = [d['step'] for d in history]
+else:
+    # or specify processing parameters manually
+    #
+    # These lists can be empty, in which case no steps will be processed.
+    # If you want to process all steps, you can use the commented lines below.
+    # The steps are specified by their step number, which is defined in the history section of the configuration file.
+    #
+    # Often you will want to work on single step, and iteratively change the parameters in the 
+    # interpretation section, until you finde the best fit for the data.
 
-#steps_to_overview = [] # range(26,37,1) # []
-#steps_to_overview = [15,16,17,18,19,20,21,22,23]
-steps_to_overview = [d['step'] for d in history]    
+    steps_to_interpret = []
+    steps_to_plot =  [] 
+    steps_to_overview = []
+    steps_to_save = []
 
-#steps_to_save = []
-steps_to_save = [d['step'] for d in history]
+    # Specify which steps to process or plot
+    #steps_to_interpret = []
+    steps_to_interpret = [22, 25] 
+    #steps_to_interpret = [d['step'] for d in interpret]   # interpret all steps defined in `interpret`
 
-plot_full_timeseries = True
-plot_full_timeseries_temp = True
-close_figs_after_save = False
+    #steps_to_plot = []
+    steps_to_plot =  [22, 25]
+    #steps_to_plot = [7,9,10,11,12,17]
+    #steps_to_plot = [d['step'] for d in history]          # plot all steps defined in `history`
 
+    #steps_to_overview = [] # range(26,37,1) # []
+    steps_to_overview = [22, 25]
+    #steps_to_overview = [d['step'] for d in history]      # plot overview of all steps defined in `history`
+
+    #steps_to_save = []
+    steps_to_save = [22, 25]
+    #steps_to_save = [d['step'] for d in history]          # save all steps defined in `history`
+
+    plot_full_timeseries = False         # If True, plot the full time series of the LVDT data
+    plot_full_timeseries_temp = False    # If True, add temperature data to the full time series plot
+    close_figs_after_save = False        # If True, close figures after saving them to file
+
+
+def convert_input_to_list_of_steps(steps, step_list):
+    """Convert argument to a list of integers."""
+    if isinstance(steps, str):
+        if steps.lower() == 'none':
+            return []
+        elif steps.lower() == 'all':
+            return [d['step'] for d in step_list]
+        else:
+            raise ValueError(f"Unknown value for steps: {steps}. "
+                                "Use 'none', 'all', or a list of step numbers.")
+    elif isinstance(steps, list):
+        return steps
+    else:
+        raise TypeError("Steps must be a string or a list of integers.")
+
+
+steps_to_interpret = convert_input_to_list_of_steps(steps_to_interpret, interpret)
+steps_to_plot =  convert_input_to_list_of_steps(steps_to_plot, history)
+steps_to_overview = convert_input_to_list_of_steps(steps_to_overview, history)
+steps_to_save = convert_input_to_list_of_steps(steps_to_save, history)
+
+#------------------------------------------------------------
 
 # create all output dirs specified in the config section of the configuration file.
-pyoedio.create_dirs([v for k,v in config.items() if 'path' in k])
-
+pyoedio.create_dirs([v for k,v in info['paths'].items() if 'path' in k])
 
 # ---------------------------------------------------------------
 # DO:
@@ -70,31 +122,82 @@ pyoedio.create_dirs([v for k,v in config.items() if 'path' in k])
 # ---------------------------------------------------------------
 
 # Iterate over all steps
-params_list = []
+step_params = {}
+step_timeseries = {}
+
+
+# ---------------------------------------------------------------
+# Do basic processing of all time steps
+# ---------------------------------------------------------------
+
+for step_id, hist in enumerate(history):
+
+    print('Basic processing {0:.0f} kPa, {1:.0f} C'.format(hist['load'], hist['temp']))
+
+    # Estract data for the current step
+    hist['name'] = info['sample']['name']
+    
+    # Select data for the current step
+    [lvdt_step, pt100_step, hobo_step] = select_data([lvdt_dat, pt100T, hoboT], hist)
+
+    # add a new column to each dataframe, containing time in minutes since the start of the load step
+    lvdt_step = add_minutes(lvdt_step)
+    pt100_step = add_minutes(pt100_step)
+    hobo_step = add_minutes(hobo_step)
+
+    params = {'step': hist['step'], 
+              'load': hist['load'], 
+              'temp': hist['temp'], 
+              'h0': sample_info['h0']}
+
+    h0 = sample_info['h0']   # mm     What thickness should be used here?
+
+    int_id = get_key_index(interpret, 'step', hist['step']) # get the index of the 
+    if int_id is not None:
+        # Extract basic parameters from the strain data
+        params.update(basic_interpretation(lvdt_step, interpret[int_id], history, params))
+
+        # Calculate the actual temperature for the current step
+        if 'temp' in interpret[int_id]:
+            params['temp'] = get_step_temp(pt100_step, interpret[int_id]['temp'])
+
+    # store the parameters for later use and processing
+    step_params[hist['step']] = params
+    # store the extracted timeseries for later use and processing
+    step_timeseries[hist['step']] = {'lvdt': lvdt_step,
+                                     'pt100': pt100_step,
+                                     'hobo': hobo_step}
+
+# ----------------------------------------------------------------------
+# Do detailed processing, plotting, and saving of specified time steps
+# ----------------------------------------------------------------------
 
 for step_id, hist in enumerate(history):
     
+    # Estract data for the current step
     hist['name'] = info['sample']['name']
     
+    # Check if the step is in the lists of steps to process
+    # if it is, extract relevant data for that step
     if (hist['step'] in steps_to_overview or 
         hist['step'] in steps_to_plot or 
         hist['step'] in steps_to_interpret or
         hist['step'] in steps_to_save):
-        # select the data
-        [lvdt_step, pt100_step, hobo_step] = select_data([lvdt_dat, pt100T, hoboT], hist)
-        lvdt_step = add_minutes(lvdt_step)
-        pt100_step = add_minutes(pt100_step)
-        hobo_step = add_minutes(hobo_step)
+
+        # Obtain the data for the current step
+        lvdt_step = step_timeseries[hist['step']]['lvdt']
+        pt100_step = step_timeseries[hist['step']]['pt100']
+        hobo_step = step_timeseries[hist['step']]['hobo']
+
+        # Obtain the basic parameters for the current step
+        params = step_params[hist['step']]
     else:
         continue
     
-    print('Processing {0:.0f} kPa, {1:.0f} C'.format(hist['load'], hist['temp']))
-    
-    params = {'step': hist['step'], 'load': hist['load'], 'temp': hist['temp'],
-              'h0': sample_info['h0']}
-    
+    print('Detailed processing {0:.0f} kPa, {1:.0f} C'.format(hist['load'], hist['temp']))
+   
     if hist['step'] in steps_to_save:
-        pyoedio.save_step(config, sample_info, hist, lvdt_step, pt100_step, hobo_step)
+        pyoedio.save_step(info['paths'], sample_info, hist, lvdt_step, pt100_step, hobo_step)
     
     if hist['step'] in steps_to_interpret:
         # do interpretation and store interpreted 
@@ -102,31 +205,69 @@ for step_id, hist in enumerate(history):
         
         h0 = sample_info['h0']   # mm     What thickness should be used here?
     
-        int_id = get_key_index(interpret, 'step', hist['step'])
+        int_id = get_key_index(interpret, 'step', hist['step']) # get the index of the current step in the interpretation dictionary
         if int_id is not None:
-            
-            params.update(basic_interpretation(lvdt_step, interpret[int_id], history, params))
-            
-            if 'temp' in interpret[int_id]:
-                params['temp'] = get_step_temp(pt100_step, interpret[int_id]['temp'])
-    
             if 'timec' in interpret[int_id]: 
+
+                # Get the intial strain for the current step
+                # If this is the first step of the experiment, eps0 should be 0.0
+                # If this is not the first step, use epsf from the previous step
+                if hist['step'] == history[0]['step']:
+                    params['eps0'] = 0
+                else:
+                    prev_step = history[step_id - 1]['step']  # Get the previous step number
+                    if prev_step in step_params:
+                        # Use epsf from the previous step as eps0 for the current step
+                        params['eps0'] = step_params[prev_step]['epsf']
+                    else:
+                        raise ValueError(f"Previous step {prev_step} not found in step_params. Could not determine eps0.")
+                
+
+                # ---------------------------------------------------------------
+                # Do the actual interpretation of the time curve 
+                # ---------------------------------------------------------------
+                
+                # Get the interpretation parameters for the current step
                 timec_info = interpret[int_id]['timec']
             
                 if timec_info['type'] == 'iso_sqrt':
-                    params = interpret_iso17892_5(lvdt_step['minutes'].values, lvdt_step['eps'].values,
-                                                    interpret[int_id], history, params)
+                    # Interpret the time curve using ISO 17892-5 method
+                    params = interpretation_iso17892_5(lvdt_step['minutes'].values, 
+                                                       lvdt_step['eps'].values,
+                                                       interpret[int_id], 
+                                                       history, 
+                                                       params)
+
+                    # Adjust the minutes in the LVDT step data to account for small (sub-second)
+                    # initial time offset, as estimated by the sqrt(t) interpretation.
+                    lvdt_step['minutes'] = lvdt_step['minutes'] - params.get('dt_eps0', 0)
+                    pt100_step['minutes'] = pt100_step['minutes'] - params.get('dt_eps0', 0)
+                    hobo_step['minutes'] = hobo_step['minutes'] - params.get('dt_eps0', 0)
+                    
+                elif timec_info['type'] == 'bh':
+                    # Interpret the time curve using Brinch Hansen method
+                    t1 = interpret[int_id]['timec'].get('t1')
+                    t2 = interpret[int_id]['timec'].get('t2')
+                    t3 = interpret[int_id]['timec'].get('t3')
+                    t4 = interpret[int_id]['timec'].get('t4')
+                    h0 = params.get('h0', sample_info['h0'])
+
+                    params = interpretation_BrinchHansen(lvdt_step['minutes'].values, 
+                                                         lvdt_step['eps'].values, 
+                                                         h0,t1, t2, t3, t4)
+
                 else:
                     raise ValueError('Unknown interpretation method!')
                 
-                params = interpret_k0(params)
+                params = calculate_k0(params)
+
 
     if hist['step'] in steps_to_overview:
         f = plot_step_overview_hobo2(lvdt_dat, pt100T, hoboT, hist)
         ovname = '{0:02.0f}_{1}_raw_{2:g}kPa_{3:g}C.png'.format(hist['step'], 
                                                                 info['sample']['name'].replace(' ','-'),
                                                                 hist['load'], hist['temp'])
-        f.savefig(os.path.join(config['savefigpath'], ovname), dpi=200)
+        f.savefig(os.path.join(info['paths']['savefigpath'], ovname), dpi=200)
         if close_figs_after_save:
             plt.close(f)
 
@@ -157,19 +298,22 @@ for step_id, hist in enumerate(history):
         tcname = '{0:02.0f}_{1}_timec_{2:g}kPa_{3:g}C.png'.format(hist['step'], 
                                                                   info['sample']['name'].replace(' ','-'),
                                                                   hist['load'], hist['temp'])
-        f.savefig(os.path.join(config['savefigpath'], tcname), dpi=200)
+        f.savefig(os.path.join(info['paths']['savefigpath'], tcname), dpi=200)
         if close_figs_after_save:
             plt.close(f)
     
-    params_list.append(params)
+    step_params[hist['step']].update(params)  # Update the step parameters with the interpreted values
 
 
 # ---------------------------------------------------------------
 # SAVE time curve interpretations to excel file
 # ---------------------------------------------------------------    
 if len(steps_to_interpret) > 0:
-    params = pd.DataFrame.from_dict(params_list)
-    params = params.set_index('step')
+    #params = pd.DataFrame.from_dict(params_list)
+    #params = params.set_index('step')
+
+    params = pd.DataFrame.from_dict(step_params, orient='index')
+    params = params.reset_index(drop=False)
 
     columns = params.columns
     order = ['load', 'temp', 'eps0', 'eps50', 'eps90', 'eps100', 'epsf', 'epss', 
@@ -179,7 +323,7 @@ if len(steps_to_interpret) > 0:
     order.extend([col for col in columns if col not in order])
     columns = [col for col in order if col in columns]
 
-    with pd.ExcelWriter(os.path.join(config['datapath'],'interpretation.xlsx')) as writer:
+    with pd.ExcelWriter(os.path.join(info['paths']['datapath'],'interpretation.xlsx')) as writer:
         params.to_excel(writer, columns=columns, sheet_name='results')
 
         txt = []
@@ -213,13 +357,14 @@ if plot_full_timeseries:
         params = pd.read_excel(latex_info['interpretation_file'], sheet_name='plotting')
         
         if plot_full_timeseries_temp:
-            Tyl = [np.floor(params['temp'].min()-1), np.ceil(params['temp'].max()+1)]
-            f.axes[1].set_ylim(Tyl)
+            pass
+            # Tyl = [np.floor(params['temp'].min()-1), np.ceil(params['temp'].max()+1)]
+            # f.axes[1].set_ylim(Tyl)
         
-            Eyl = [np.ceil(params['epsf'].max()+1), np.floor(params['epsf'].min()-1)]
-            f.axes[0].set_ylim(Eyl)
+            # Eyl = [np.ceil(params['epsf'].max()+1), np.floor(params['epsf'].min()-1)]
+            # f.axes[0].set_ylim(Eyl)
     
-    f.savefig(os.path.join(config['savefigpath'], 'lvdt_strain_full.png'), dpi=200,
+    f.savefig(os.path.join(info['paths']['savefigpath'], 'lvdt_strain_full.png'), dpi=200,
               bbox_inches='tight')
     if close_figs_after_save:
         plt.close(f)    
@@ -233,13 +378,13 @@ if os.path.exists(latex_info['interpretation_file']):
     
     f = plot_consolidation(params)
     
-    f.savefig(os.path.join(config['savefigpath'], 'consolidation_curve.png'), 
+    f.savefig(os.path.join(info['paths']['savefigpath'], 'consolidation_curve.png'), 
           dpi=300, bbox_inches = 'tight')
           
     yl = f.axes[0].get_ylim()
     f.axes[0].set_ylim([17.5, yl[1]])
     
-    f.savefig(os.path.join(config['savefigpath'], 'consolidation_curve_yl.png'), 
+    f.savefig(os.path.join(info['paths']['savefigpath'], 'consolidation_curve_yl.png'), 
               dpi=300, bbox_inches = 'tight')
               
     if close_figs_after_save:
@@ -251,8 +396,3 @@ if os.path.exists(latex_info['interpretation_file']):
 
 pyoedio.produce_latex_file(info)
     
-    
-
-    
-
-
